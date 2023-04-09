92b77378224fa8e6d0e5bda6747c28e9e76c774b
guisena08 <guisena@ufc.alu.br>
2023-04-09 17:58:04 -0300

    segunda tentativa de resolver atividade

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ metros_quadrados = float(metros_quadrados)
 
 
 # Coloque o código para resolver o problema nessa parte do programa
-litros = float(metros_quadrados/3.0)
-
+litros = 54
 
 
 # As duas variáveis qtd_de_latas e valor_total devem guardar a resposta do problema
 # Troque os zeros pelos valores corretos.
-qtd_de_latas = int(litros/18.0)
+qtd_de_latas = int(metros_quadrados/litros) + 1
 valor_total = qtd_de_latas * 80
 
 
